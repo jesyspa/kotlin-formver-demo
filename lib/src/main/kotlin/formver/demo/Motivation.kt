5c5c0461861401431cab0e1d66ee4c74aa22714c
Anton Golov <anton.golov@jetbrains.com>
2023-10-25 11:47:42 +0200

    Fix motivational example

--- lib/src/main/kotlin/formver/demo/Motivation.kt
+++ lib/src/main/kotlin/formver/demo/Motivation.kt
@@ -1,24 +1,27 @@
+@file:Suppress("unused")
+
 package formver.demo
 
 import org.jetbrains.kotlin.formver.plugin.NeverConvert
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
 
-class Declaration(val elements: List<Any>)
+class Elements
+class Declaration(val elements: Elements)
 
 @NeverConvert
 fun find(index: Int): Any? = TODO()
+@NeverConvert
+fun noElements() = Elements()
 
 @OptIn(ExperimentalContracts::class)
-fun process(index: Int, nullIfNotFound: Boolean): List<Int>? {
+fun process(index: Int, nullIfNotFound: Boolean): Elements? {
     contract {
         returnsNotNull() implies (!nullIfNotFound)
     }
 
-    val declaration = find(index) ?: return if (nullIfNotFound) null else emptyList()
+    val declaration = find(index) ?: return if (nullIfNotFound) null else noElements()
 
-    return if (declaration !is Declaration) {
-        emptyList()
-    } else declaration.elements
-        .filterIsInstance<Int>()
+    return if (declaration !is Declaration) noElements()
+    else declaration.elements
 }
